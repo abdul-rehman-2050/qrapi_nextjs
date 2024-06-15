cc5a605eac8d1bb1a2c8c3d399f0875ee7e01a1d
Abdul Rehman <supermind2002@gmail.com>
2024-06-15 13:00:46 +0500

    getting vitals api GET Route

---
+++ src/app/apitest/vitals/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { supabase } from "../../../../lib/supabaseClient";
+
+
+export async function GET(req, res) {
+    
+
+    const { data: vitals } = await supabase
+                        .from('patient_vitals')
+                        .select('*')
+                        .order('bed_number', true);                        
+    const name = req.nextUrl.searchParams.get("name") || "World";  
+
+    //convert vitals to json response 
+    const vitalsJson = JSON.stringify(vitals);
+    return NextResponse.json(vitalsJson);
+}
